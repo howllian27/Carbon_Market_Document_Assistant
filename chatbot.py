7576a02e177afc65945ece33cab4418bcf514d2f
Howllian27 <67510209+howllian27@users.noreply.github.com>
2023-11-07 12:04:02 +0800

    added comments

--- chatbot.py
+++ chatbot.py
@@ -35,27 +35,36 @@ def interactive_pdf_qa():
     
     uploaded_pdf = st.file_uploader('Upload your PDF Document', type='pdf')
     
+    # Check if pdf is uploaded
     if uploaded_pdf:
         pdf_content_reader = PdfReader(uploaded_pdf)
         # Accumulate text content from the PDF
         accumulated_text = ""
+
+        # Iterate over all pages in the PDF
         for page in pdf_content_reader.pages:
             accumulated_text += page.extract_text()
         
         # Build the index of embeddings from the PDF text
         pdf_embedding_index = create_embedding_index_from_text(accumulated_text)
         
+        # Enter user inputs
         user_query = st.text_input('Enter your question for the PDF')
         is_cancelled = st.button('Cancel')
         
+        # Check if user has cancelled
         if is_cancelled:
             st.stop()
         
+        # Check if user has entered a query
         if user_query:
+            # Search for similar documents
             similar_documents = pdf_embedding_index.similarity_search(user_query)
             language_model = OpenAI()
+            # Load the QA chain
             qa_chain = load_qa_chain(language_model, chain_type='stuff')
             
+            # Run the QA chain and track the cost
             with get_openai_callback() as cost_tracker:
                 answer = qa_chain.run(input_documents=similar_documents, question=user_query)
                 print(cost_tracker)
